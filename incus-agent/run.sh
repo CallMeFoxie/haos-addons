1f437fd2f753cc9a0c2847c6f2e2b6b6949a8027
Ashley CallMeFoxie <ashley@victorianfox.com>
2025-08-31 12:23:44 +0200

    Fix6

--- incus-agent/run.sh
+++ incus-agent/run.sh
@@ -9,9 +9,9 @@ if [ ! -f ${AGENT_PATH}/agent.crt ]; then
     mkdir -p /tmp/incusmnt
     mount -t 9p config /tmp/incusmnt
     mkdir -p ${AGENT_PATH}
-    for i in agent.crt agent.key server.crt incus-agent; do
-        cp /tmp/incusmnt/${i} ${AGENT_PATH}/
-    done
+#    for i in agent.crt agent.key server.crt incus-agent; do
+    cp -rvp /tmp/incusmnt/* ${AGENT_PATH}/
+#    done
     echo "Setting 0600 on agent key"
     chmod 0600 ${AGENT_PATH}/agent.key
     echo "Unmounting"
